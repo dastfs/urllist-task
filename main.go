c3faaba7b23b5129f1c206e687b7bc16dbd0a04f
dastano <dastano@halykbank.kz>
2020-06-01 16:58:30 +0600

    signals

--- main.go
+++ main.go
@@ -1,4 +1,41 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+
+	sigs := make(chan os.Signal, 1)
+
+	// `signal.Notify` registers the given channel to
+	// receive notifications of the specified signals.
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	msg := make(chan string, 1)
+	go func() {
+		// Receive input in a loop
+		for {
+			var s string
+			fmt.Scan(&s)
+			// Send what we read over the channel
+			msg <- s
+		}
+	}()
+
+loop:
+	for {
+		select {
+		case <-sigs:
+			fmt.Println("Got shutdown, exiting")
+			// Break out of the outer for statement and end the program
+			break loop
+		case s := <-msg:
+			fmt.Println("Echoing: ", s)
+		}
+	}
+
 }
